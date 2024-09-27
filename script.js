04c74dad326f62e0dae33395f1141cefafece94d
0somesh <0somesh.sh@gmail.com>
2024-09-27 23:42:24 +0530

    updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const name = 'John Doe';
 const DOB = '2/2/3';
 
 function sayName(){
-    console.log('MayName' , + 1name);
+    console.log('MayName' , + this.name);
 }
 
 const calculate = (a,b, operation) => {
